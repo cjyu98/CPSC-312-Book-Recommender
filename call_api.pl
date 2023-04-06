f09eba484ee3d3bc7faa38bf528fdc91f7c26e5b
cjyu98 <cjyu98@gmail.com>
2023-04-06 01:01:06 -0700

    updated api

--- call_api.pl
+++ call_api.pl
@@ -1,8 +1,14 @@
-% testing file for api call, to be adjusted as we go on
+% calls and prints the results from calling the API
 
 % library - will likely need to move into main file
 :- use_module(library(http/http_open)).
 :- use_module(library(http/json)).
+:- use_module(library(lists)).
+
+% encoding to process the text & accents from csv file properly
+% reference: https://swi-prolog.discourse.group/t/pcemacs-strips-some-accents-on-save-solved/1595/11
+:- set_prolog_flag(encoding, utf8).
+
 
 % Constants
 apikey("AIzaSyCDCSCwN5M95LfkkLtw5wQ8kkaRA-wBYqM"). % linked to Carmen's Google account
@@ -11,38 +17,60 @@ url_root("https://www.googleapis.com/books/v1/volumes?q=").
 
 % current test case: 
     % calling the api for searching the book "The Goldfinch"
-    % for now, just printing out descriptions of top results as we test
-    % will generalize later
     % testing URL: https://www.googleapis.com/books/v1/volumes?q=the+goldfinch&key=AIzaSyCDCSCwN5M95LfkkLtw5wQ8kkaRA-wBYqM
     % click URL to see how the JSON file returns
     % query: call_api("https://www.googleapis.com/books/v1/volumes?q=the+goldfinch&key=AIzaSyCDCSCwN5M95LfkkLtw5wQ8kkaRA-wBYqM").
     % every search term we parse must append a +
 
-% call the api, printing out the book descriptions
+% call the api to print out the top 5 results
 call_api(URL) :- 
-    write("calling api\n"),
+    write('Calling GoogleBooks API'),
+    write("\n\n"),
     http_open(URL, Input_stream, []),
  	json_read_dict(Input_stream, Dict),
  	close(Input_stream),
     json_to_book_description(Dict, Books),
-    write_description(Books).
+    write_results(Books).
 
+% reads the key-value pairs in the JSON dict
+json_to_book_description(Dict, Dict.items). 
 
-json_to_book_description(Dict, Dict.items). % reads the key-value pairs in the JSON dict
+% prints out the title, author and description of the results from API search
+write_results([]). % base case
+write_results([Book|T]) :-
 
-% prints out the description of the books
-write_description([]).
-write_description([Book|T]) :-
+    % assign the values
+    Title = Book.volumeInfo.title,
+    Author = Book.volumeInfo.authors,
     Description = Book.volumeInfo.description,
-    string_concat(Description, "\n\n", Description1),
-    write(Description1),
-    write_description(T).
 
+    % writes title
+    string_concat('Title: ', Title, Title1),
+    writeln(Title1),
+
+    % writes author
+    write('Author(s): '),
+    write_author(Author),
+    write("\n"),
+
+    % writes description
+    string_concat('Description: ', Description, Description1),
+    write(Description1),
+    write("\n\n"),
+
+    % recursive call
+    write_results(T).
+
+% writes out the authors since it is a list
+write_author([]).
+write_author([H|T]) :-
+    write(H),
+    write(","),
+    write_author(T).
 
 % TODO:
-% clean up the API function
 % have it print only top 5 results (URL would have & maxResults=5 added)
-% have it print title, author and description
+
 
  
 
